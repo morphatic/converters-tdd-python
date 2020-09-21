fd76907714bd013185fab83a94fab842293fe9a8
Morgan Benton <registered@morphatic.com>
2020-09-21 18:20:16 -0400

    pytest ini; pylint ok; smoke ok

---
+++ converters_test.py
@@ -0,0 +1,7 @@
+"""Unit tests for the units converter library functions"""
+
+def describe_a_library_of_units_converters():
+    """Test suite for units conversion functions"""
+    # pylint: disable=unused-variable
+    def blows_smoke():
+        assert True
