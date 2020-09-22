a2dd3730d98a3b1a8f3dc190f442747f1c1c6c0e
Morgan Benton <registered@morphatic.com>
2020-09-22 15:30:17 -0400

    tested 4 converters

--- converters.py
+++ converters.py
@@ -1,10 +1,14 @@
 """Library of units converters"""
 
 # Declare useful constants
-NPL = 4.44822 # newtons per pound-foot
-IPM = 1550 # square inches per square meter
-LPN = 0.224809 # pound-force per newton
+NPL = 4.44822    # newtons per pound-foot
+IPM = 1550       # square inches per square meter
+LPN = 0.224809   # pound-force per newton
 MPI = 0.00064516 # meters squared per inch squared
+MPK = 0.621372   # miles per kilometer
+LPG = 3.78541    # liters per gallon
+KPM = 1.60934    # kilometers per mile
+GPL = 0.264172   # gallons per liter
 
 def psi2kpa(psi):
     """
@@ -17,3 +21,15 @@ def kpa2psi(kpa):
     Takes in a KPa value and converts it to PSI.
     """
     return kpa * 1000 * LPN * MPI
+
+def mpg2lp100k(mpg):
+    """
+    Takes in a MPG value and converts it to liters per 100km.
+    """
+    return (1/mpg) * MPK * LPG * 100
+
+def lp100k2mpg(lp100k):
+    """
+    Takes in liters per 100km value and converts it to mpg.
+    """
+    return 1/(lp100k * 0.01 * KPM * GPL)
